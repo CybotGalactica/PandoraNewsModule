94e9f99b5d75d71ca7218e5d4e7406d916479adc
Simon Struck <simon@titan.powersrv.de>
2018-05-15 21:02:39 +0200

    Fix Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-echo Uploading...
-scp build/libs/PandoraTracker-2.0-SNAPSHOT.jar titan:pandorabot/official/modules/
-echo Done!
+cp build/libs/PandoraTracker-2.0-SNAPSHOT.jar ../official/modules/
+
